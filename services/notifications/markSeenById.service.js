97b8c00d875f99cd94691157203913d9620f27ab
furkansenkal <furkansenkal@hotmail.com>
2025-04-01 18:17:42 +0300

    fix: wrong file name fixed

--- services/notifications/markSeenById.service.js
+++ services/notifications/markSeenById.service.js
@@ -1,4 +1,4 @@
-import Notification from "../../db/models/Notification.js";
+import Notification from "../../db/models/notification.js";
 export default async function markSeenById({ notification_id }) {
   try {
     const notification = await Notification.findByIdAndUpdate(
