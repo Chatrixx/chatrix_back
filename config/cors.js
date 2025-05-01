5781194a83b99a06e87e3f296d773a02650ce11d
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2025-05-01 07:04:47 +0300

    config: cors test

--- config/cors.js
+++ config/cors.js
@@ -1,26 +1,17 @@
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://chatrix-v1.vercel.app",
-//   "https://hkaratas.com",
-// ];
-
-// export const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)
-//     ) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true, // allow cookies or auth headers
-//   exposedHeaders: ["Authorization"], // allow frontend access to this header
-// };
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://chatrix-v1.vercel.app",
+  "https://hkaratas.com",
+];
 
 export const corsOptions = {
-  origin: (origin, callback) => callback(null, origin || "*"), // reflect any origin
-  credentials: true, // allow cookies, auth headers
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"], // allow all methods
-  allowedHeaders: ["Content-Type", "Authorization"], // allow common headers
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // allow cookies or auth headers
   exposedHeaders: ["Authorization"], // allow frontend access to this header
 };
