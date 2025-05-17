5d8bb706a3c205c581c71b9b7484a22f573fe44f
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2025-05-17 20:25:58 +0300

    config: Cors update

--- config/cors.ts
+++ config/cors.ts
@@ -2,17 +2,24 @@ import { CorsOptions } from "cors";
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://chatrix-v1.vercel.app",
+  "https://myfronturl",
   "https://hkaratas.com",
 ];
 
 export const corsOptions: CorsOptions = {
   origin: (requestOrigin, callback) => {
-    if (!requestOrigin || allowedOrigins.includes(requestOrigin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    return (req: Request) => {
+      const userAgent = req.headers.get("user-agent") || "";
+
+      if (userAgent === "ManyChat") {
+        // Allow regardless of origin
+        callback(null, true);
+      } else if (!requestOrigin || allowedOrigins.includes(requestOrigin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    };
   },
   credentials: true,
   exposedHeaders: ["Authorization"],
